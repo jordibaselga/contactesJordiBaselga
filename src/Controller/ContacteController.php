676fa5ceda5079d598ea3f1ba2f6a9864c73480a
jordibaselga <jobaselga@gmail.com>
2021-02-10 21:30:43 +0100

    pdf 3

---
+++ src/Controller/ContacteController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContacteController extends AbstractController
+{
+    private $contactes = array(
+        array(
+            "codi" => 1, "nom" => "Salvador Sala",
+            "telefon" => "638961244", "email" => "salvasala@simarro.org"
+        ),
+        array(
+            "codi" => 2, "nom" => "Anna Llopis",
+            "telefon" => "669332004", "email" => "annallopis@simarro.org"
+        ),
+        array(
+            "codi" => 3, "nom" => "Marc Sanchis",
+            "telefon" => "962286040", "email" => "marcsanchis@simarro.org"
+        ),
+        array(
+            "codi" => 4, "nom" => "Laura Palop",
+            "telefon" => "663568890", "email" => "laurapalop@simarro.org"
+        ),
+        array(
+            "codi" => 5, "nom" => "Sara Sidle",
+            "telefon" => "638765434", "email" => "sarasidle@simarro.org"
+        ),
+    );
+    /**
+     * @Route("/contacte/{codi}", name="fitxa_contacte", requirements={"codi"="\d+"})
+     */
+    public function fitxa($codi)
+    {
+        $resultat = array_filter(
+            $this->contactes,
+            function ($contacte) use ($codi) {
+                return $contacte["codi"] == $codi;
+            }
+        );
+        if (count($resultat) > 0) {
+            return $this->render('fitxa_contacte.html.twig', array('contacte' => array_shift(($resultat))));
+        } else
+            return $this->render('fitxa_contacte.html.twig', array('contacte' => NULL));
+    }
+
+    /**
+     * @Route("/contacte/{text}", name="buscar_contacte")
+     */
+    public function buscar($text)
+    {
+        $resultat = array_filter(
+            $this->contactes,
+            function ($contacte) use ($text) {
+                return strpos($contacte["nom"], $text) !== FALSE;
+            }
+        );
+        
+            return $this->render('llista_contactes.html.twig', array('contacte' => array_shift(($resultat))));
+    }
+}
---
+++ src/Controller/IniciController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class IniciController extends AbstractController
+{
+/**
+* @Route("/", name="inici")
+*/
+public function inici()
+{
+return $this->render('inici.html.twig');
+}
+}
+?>
